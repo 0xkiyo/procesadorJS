0a5aa32f0d302315f19efd7b577ae9a801c1ab8a
Kiyoshi Omaza <kiyoshi.osaldana@gmail.com>
2017-12-06 14:10:22 +0100

    Nueva estructura.

---
+++ src/error/EmparejaException.java
@@ -0,0 +1,13 @@
+package error;
+
+public class EmparejaException extends Exception {
+
+    public EmparejaException() {
+
+    }
+
+    public EmparejaException(String message) {
+        super(message);
+    }
+
+}
