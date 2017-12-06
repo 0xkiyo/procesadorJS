dd3a4aa871b3a0665ce996af45eb1c6741d3468e
Kiyoshi Omaza <kiyoshi.osaldana@gmail.com>
2017-12-06 14:05:13 +0100

    Corregido errores Lexico.java

--- Lexico.java
+++ Lexico.java
@@ -43,7 +43,6 @@ public class Lexico {
 			this.fr.read(a); //añade el contenido al array
 
 			String ruta = Sintactico.miDir.getCanonicalPath()+"\\tokens.txt";
-			String ruta = Lexico.miDir.getCanonicalPath()+"\\tokens.txt";
 
 			File archivoTokens = new File(ruta);
 			this.bw = new BufferedWriter(new FileWriter(archivoTokens));//leemos el fichero
