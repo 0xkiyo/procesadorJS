62ad2dabb8e16076e611ba8dd4407cf57faf4eb5
Javier Gamarra <nhpatt@gmail.com>
2018-01-29 08:10:54 +0100

    small refactors

--- ProcesadorJSV/src/errores/ParserException.java
+++ ProcesadorJSV/src/errores/ParserException.java
@@ -1,12 +1,20 @@
 package errores;
 
-public class ParserException extends Exception {
+public class ParserException extends RuntimeException {
+
+    private Reason reason;
+
     public enum Reason {
-        CADENA, CODIGO_MUERTO, COMENTARIO, CONCATENACION_NO_IMPLEMENTADA, DECLARACION_INCOMPATIBLE, DEVUELVE_CADENA, EMPAREJA,
-        FIRST_NO_COINCIDE, FUERA_DE_RANGO, FUNCION_NO_DECLARADA, ID, OP_LOGICO, TIPO_INCORRECTO, TIPOS_DIFERENTES, OTRO_SIMBOLO, VARIABLE_NO_DECLARADA
+        CADENA, COMENTARIO, CONCATENACION_NO_IMPLEMENTADA, DECLARACION_INCOMPATIBLE, DEVUELVE_CADENA, EMPAREJA,
+        FIRST_NO_COINCIDE, FUERA_DE_RANGO, FUNCION_NO_DECLARADA, ID, OP_LOGICO, TIPO_INCORRECTO, TIPOS_DIFERENTES, OTRO_SIMBOLO
     }
 
     public ParserException(Reason reason, String message) {
         super(message);
+        this.reason = reason;
+    }
+
+    public Reason getReason() {
+        return reason;
     }
 }
